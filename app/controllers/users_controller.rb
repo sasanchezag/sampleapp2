b2fb2fff5e66220b207b64164fe5d20d7926039e
SantiagoSa <santiago.sanchez@kommit.co>
2019-09-25 13:10:48 -0500

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,31 @@
 class UsersController < ApplicationController
-  def new
-  	puts "\n"*10
-  	puts "qa"
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  	puts "\n" * 5
+  	puts "XXX" * 10
+  	puts params
+    @user = User.new(user_params)
+    if @user.save
+    	puts "guarde usuario"
+    	flash[:success] = "Welcome to the Sample App!"
+    	redirect_to @user
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp2::Application.routes.draw do
 
+  resources :users
   root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
   
 
   #match '/signup',  to: 'users#new',            via: 'get'
