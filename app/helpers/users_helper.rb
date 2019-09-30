868152a1778647f818308ce76ad2333fd7dae550
SantiagoSa <santiago.sanchez@kommit.co>
2019-09-30 16:03:32 -0500

    Finish user edit, update, index, and destroy actions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,13 @@
 module UsersHelper
 
   # Returns the Gravatar (http://gravatar.com/) for the given user.
-  def gravatar_for(user)
+  def gravatar_for(user, size = 52)
+  	puts "\n" * 5
+  	puts "XXX" * 10
+  	puts size
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
+
   end
 end
